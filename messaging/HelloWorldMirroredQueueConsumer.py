99d78ab4a0d35000bbf83d4f45bbb256fd1e073a
junwen.luo <luojunwenbashen@163.com>
2018-03-20 22:47:41 +0800

    add example for declaring mirrored queue

---
+++ messaging/HelloWorldMirroredQueueConsumer.py
@@ -0,0 +1,33 @@
+import rabitconfig
+import pika
+
+channel = rabitconfig.getDefaultChangel()
+
+channel.exchange_declare(
+    exchange="hello-exchange",
+    exchange_type="direct",
+    passive=False,
+    durable=True,
+    auto_delete=False)
+
+# here is the difference for mirrored queue
+queue_args = {"x-ha-policy": "all"}
+channel.queue_declare(queue="hello-queue", arguments=queue_args)
+channel.queue_bind(
+    queue="hello-queue", exchange="hello-exchange", routing_key="hola")
+
+
+def msg_consumer(channel, method, header, body):
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+    if body == "quit":
+        channel.basic_cancel(consumer_tag="hello-consumer")
+        channel.stop_consuming()
+    else:
+        print body
+
+    return
+
+
+channel.basic_consume(
+    msg_consumer, queue="hello-queue", consumer_tag="hello-consumer")
+channel.start_consuming()
